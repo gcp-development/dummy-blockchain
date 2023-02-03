739698327d87fb10dbe61d3117c20c99db8cc975
gcp-development <bruno.silveira.backup@gmail.com>
2023-02-03 15:56:13 +0000

    Update libp2p to version 0.50

--- node-app/src/main.rs
+++ node-app/src/main.rs
@@ -1,41 +1,41 @@
+use futures::StreamExt;
 use libp2p::{
-    futures::StreamExt,
+    //gossipsub,
     identity,
-    //mdns,
-    mdns::{Mdns, MdnsConfig, MdnsEvent},
-    swarm::{Swarm, SwarmEvent},
+    mdns,
+    //swarm::NetworkBehaviour,
+    swarm::SwarmEvent,
     PeerId,
+    Swarm,
 };
 use std::error::Error;
 
 static POD_PORT: &str = "4242";
 
-#[tokio::main]
+#[async_std::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let id_keys = identity::Keypair::generate_ed25519();
     let peer_id = PeerId::from(id_keys.public());
-
     let transport = libp2p::development_transport(id_keys).await?;
-    let behaviour = Mdns::new(MdnsConfig::default()).await?;
-    let mut swarm = Swarm::new(transport, behaviour, peer_id);
+    let behaviour = mdns::async_io::Behaviour::new(mdns::Config::default())?;
+    let mut swarm = Swarm::with_async_std_executor(transport, behaviour, peer_id);
     let mut multi_address_pod: String = "/ip4/0.0.0.0/tcp/".to_owned();
 
     multi_address_pod.push_str(&POD_PORT);
-
     println!("Local peer id: {:?}", peer_id);
-    println!("Node a Multiaddress: {}", multi_address_pod);
+    println!("Pod Multi address: {}", multi_address_pod);
     swarm.listen_on(multi_address_pod.parse()?)?;
     loop {
         match swarm.select_next_some().await {
             SwarmEvent::NewListenAddr { address, .. } => {
                 println!("Listening on local address {:?}", address)
             }
-            SwarmEvent::Behaviour(MdnsEvent::Discovered(peers)) => {
+            SwarmEvent::Behaviour(mdns::Event::Discovered(peers)) => {
                 for (peer, addr) in peers {
                     println!("discovered {} {}", peer, addr);
                 }
             }
-            SwarmEvent::Behaviour(MdnsEvent::Expired(expired)) => {
+            SwarmEvent::Behaviour(mdns::Event::Expired(expired)) => {
                 for (peer, addr) in expired {
                     println!("expired {} {}", peer, addr);
                 }
