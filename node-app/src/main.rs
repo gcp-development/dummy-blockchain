501b4631545dea31149cf56979076ae5dad57276
gcp-development <bruno.silveira.backup@gmail.com>
2023-02-03 12:23:21 +0000

    mDNS discovery

--- node-app/src/main.rs
+++ node-app/src/main.rs
@@ -2,31 +2,29 @@ use libp2p::{
     futures::StreamExt,
     identity,
     //mdns,
-    //mdns::{Mdns, MdnsConfig, MdnsEvent},
+    mdns::{Mdns, MdnsConfig, MdnsEvent},
     swarm::{Swarm, SwarmEvent},
     PeerId,
 };
 use std::error::Error;
 
+static POD_PORT: &str = "4242";
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-
     let id_keys = identity::Keypair::generate_ed25519();
     let peer_id = PeerId::from(id_keys.public());
-    println!("Local peer id: {:?}", peer_id);
+
     let transport = libp2p::development_transport(id_keys).await?;
-
-    // Create an MDNS network behaviour.
-    //libp2p::mdns::async_io::Mdns
-    //libp2p::mdns::tokio
-    let behaviour = mdns::async_io::Behaviour::new(mdns::Config::default(), peer_id)?;
-
-/*
-    //let behaviour = Mdns::new(MdnsConfig::default()).await?;
-
+    let behaviour = Mdns::new(MdnsConfig::default()).await?;
     let mut swarm = Swarm::new(transport, behaviour, peer_id);
+    let mut multi_address_pod: String = "/ip4/0.0.0.0/tcp/".to_owned();
 
-    swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
+    multi_address_pod.push_str(&POD_PORT);
+
+    println!("Local peer id: {:?}", peer_id);
+    println!("Node a Multiaddress: {}", multi_address_pod);
+    swarm.listen_on(multi_address_pod.parse()?)?;
     loop {
         match swarm.select_next_some().await {
             SwarmEvent::NewListenAddr { address, .. } => {
@@ -45,6 +43,4 @@ async fn main() -> Result<(), Box<dyn Error>> {
             _ => {}
         }
     }
-     */
-    Ok(())
 }
